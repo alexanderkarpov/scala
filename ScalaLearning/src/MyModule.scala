29af75ddeb964bc4286baddf3e637d4f25aaba57
Alexander Karpov <alexander.karpoff@gmail.com>
2015-01-04 19:47:10 +0300

    factorial (tail recursive)

--- ScalaLearning/src/MyModule.scala
+++ ScalaLearning/src/MyModule.scala
@@ -1,6 +1,3 @@
-/**
- * Created by alexander on 1/4/15.
- */
 object MyModule {
 
   def abs(n: Int): Int = {
@@ -10,11 +7,21 @@ object MyModule {
 
   private def formatAbs(x: Int): String = {
     val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+    msg.format(x, this.abs(x))
+  }
+
+  def factorial(n: Int):Long = {
+    def go (n: Int, acc: Long): Long = {
+      if (n<=0) acc
+      else go(n-1,n*acc)
+    }
+
+    go(n, 1)
   }
 
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(MyModule abs -7)
+    println(factorial(7))
   }
-
 }
