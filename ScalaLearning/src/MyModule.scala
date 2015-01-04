be081d49517231bf0057f1a1001f5e1684e571c3
Alexander Karpov <alexander.karpoff@gmail.com>
2015-01-04 21:18:22 +0300

    fibonacci

--- ScalaLearning/src/MyModule.scala
+++ ScalaLearning/src/MyModule.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object MyModule {
 
   def abs(n: Int): Int = {
@@ -11,17 +13,28 @@ object MyModule {
   }
 
   def factorial(n: Int):Long = {
-    def go (n: Int, acc: Long): Long = {
+    @tailrec
+    def go(n: Int, acc: Long): Long = {
       if (n<=0) acc
       else go(n-1,n*acc)
     }
-
     go(n, 1)
+  }
+
+
+  def fibonacci(n: Int):Int ={
+    @tailrec
+    def go(n: Int, value1: Int, value2: Int): Int = {
+      if(n<=1) value2
+      else go(n-1,value2,value1+value2)
+    }
+    go(n,0,1)
   }
 
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(MyModule abs -7)
     println(factorial(7))
+    println(fibonacci(9))
   }
 }
