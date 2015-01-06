941c21e5555e5fcf2a90174bea22b49def4687f3
Alexander Karpov <alexander.karpoff@gmail.com>
2015-01-06 11:50:53 +0300

    ex 2.3-2.5

--- ScalaLearning/src/MyModule.scala
+++ ScalaLearning/src/MyModule.scala
@@ -67,8 +67,18 @@ object MyModule {
   into a function of one argument that partially applies f . Here again there’s only one
   implementation that compiles. Write this implementation.
    */
-  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+  def curry[A,B,C](f:(A,B) => C): A => (B => C) = {
+    (a:A)=> (b:B) => f(a,b)
+  }
 
+  //EXERCISE 2.4
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+    (a:A, b:B) => f(a)(b)
+  }
+
+  //EXERCISE 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    (a:A) => f(g(a))
   }
 
 
